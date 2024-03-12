f9147fe154b5dfba49a9f9142db3ef2c1f3f61da
aliakbarpirnazar <aliakbarpirnazar@gmail.com>
2024-03-12 11:22:18 +0330

    final V1

--- rjs-divar-files-main/src/components/templates/CheckOtpForm.jsx
+++ rjs-divar-files-main/src/components/templates/CheckOtpForm.jsx
@@ -1,6 +1,16 @@
-import { checkOtp } from "../../services/auth";
+import { useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+
+import { checkOtp } from "services/auth";
+import { getProfile } from "services/user";
+import { setCookie } from "utils/cookie";
+
+import styles from "./CheckOtpForm.module.css";
 
 function CheckOtpForm({ code, setCode, setStep, mobile }) {
+  const navigate = useNavigate();
+  const { refetch } = useQuery(["profile"], getProfile);
+
   const submitHandler = async (event) => {
     event.preventDefault();
     if (code.length !== 5) return;
@@ -8,12 +18,14 @@ function CheckOtpForm({ code, setCode, setStep, mobile }) {
     const { response, error } = await checkOtp(mobile, code);
 
     if (response) {
-      console.log(response);
+      setCookie(response.data);
+      navigate("/");
+      refetch();
     }
     if (error) console.log(error.response.data.message);
   };
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} className={styles.form}>
       <p>تایید کد اس ام اس شده</p>
       <span> کد پیامک شده به شماره {mobile} وارد کنید.</span>
       <label htmlFor="input">کد تایید را وارد کنید</label>
@@ -25,7 +37,9 @@ function CheckOtpForm({ code, setCode, setStep, mobile }) {
         onChange={(e) => setCode(e.target.value)}
       />
       <button type="submit">ورود</button>
-      <button onClick={() => setStep(1)}>تغییر شماره موبایل</button>
+      <button onClick={() => setStep(1)} className={styles.backButton}>
+        تغییر شماره موبایل
+      </button>
     </form>
   );
 }
--- rjs-divar-files-main/src/components/templates/SendOtpForm.jsx
+++ rjs-divar-files-main/src/components/templates/SendOtpForm.jsx
@@ -1,4 +1,6 @@
-import { sendOtp } from "../../services/auth";
+import { sendOtp } from "services/auth";
+
+import styles from "./SendOtpForm.module.css";
 
 function SendOtpForm({ mobile, setMobile, setStep }) {
   const submitHandler = async (event) => {
@@ -10,7 +12,7 @@ function SendOtpForm({ mobile, setMobile, setStep }) {
     if (error) console.log(error.response.data.message);
   };
   return (
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} className={styles.form}>
       <p>ورود به حساب کاربری</p>
       <span>
         برای استفاده از امکانات دیوار،لطفا شماره موبایل خود را وارد کنید. کد
