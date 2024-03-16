4290b889a9f65835371d991286e81e942f4f1756
aliakbarpirnazar <aliakbarpirnazar@gmail.com>
2024-03-16 12:36:19 +0330

    Final version

--- rjs-divar-files-main/src/pages/HomePage.jsx
+++ rjs-divar-files-main/src/pages/HomePage.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "components/templates/Sidebar";
+import Main from "components/templates/main";
+import { useQuery } from "@tanstack/react-query";
+import { getAllPosts } from "services/user";
+import Loader from "components/modules/Loader";
+import { getCategory } from "services/admin";
+
+const style = { display: "flex" };
 
 function HomePage() {
+  const { data: posts, isLoading: postLoading } = useQuery(
+    ["post-List"],
+    getAllPosts
+  );
+  const { data: categories, isLoading: categoryLoading } = useQuery(
+    ["get-categories"],
+    getCategory
+  );
+
   return (
-    <div>HomePage</div>
-  )
+    <>
+      {postLoading || categoryLoading ? (
+        <Loader />
+      ) : (
+        <div style={style}>
+          <Sidebar categories={categories} />
+          <Main posts={posts} />
+        </div>
+      )}
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
